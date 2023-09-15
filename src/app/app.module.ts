2bc0da2d9fc43ed7338f5adfe88fe412970de694
karthikb01 <karthikb1903@gmail.com>
2023-09-15 23:09:45 +0530

    contact changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -17,6 +21,10 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+
 
 @Component({
   selector: 'app-landing',
@@ -7,4 +12,55 @@ import { Component } from '@angular/core';
 })
 export class LandingComponent {
 
+  constructor(private dialog : MatDialog) {}
+
+  @ViewChild('contact') form! : NgForm
+
+  message = {
+    name : '',
+    email : '',
+    subject : '',
+    message : ''
+  }
+
+  wait : boolean = false
+
+  public sendEmail(e: Event) {
+    this.wait = true
+    e.preventDefault();
+    emailjs.sendForm('service_e2j02t9', 'template_y7q1ct8', e.target as HTMLFormElement, '_dpS08DVNN0fslLh5')
+      .then((result: EmailJSResponseStatus) => {
+        // console.log(result.text);        
+        if(result.text == "OK"){
+          this.wait = false
+          this.form.reset()
+          this.dialog.open(SuccessDialog, {restoreFocus : false});
+        }
+      }, (error) => {
+        // console.log(error.text);
+        this.wait = false
+        this.dialog.open(ErrorDialog, {restoreFocus : false})
+      });
+  }
+
 }
+
+@Component({
+  selector: 'success-dialog',
+  templateUrl: 'dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class SuccessDialog {}
+
+@Component({
+  selector: 'error-dialog',
+  template: `<h1 mat-dialog-title>Error</h1>
+  <div mat-dialog-content>Some error has occured!</div>
+  <div mat-dialog-actions>
+    <button mat-button mat-dialog-close>Close</button>
+  </div>`,
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class ErrorDialog {}
