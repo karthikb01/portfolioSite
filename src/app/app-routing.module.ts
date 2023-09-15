837f7f3c303676e1bd46842bfab4235d1e8b5804
karthikb01 <karthikb1903@gmail.com>
2023-09-15 17:16:49 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+
+const routes: Routes = [
+  {
+    path : 'home',
+    component : LandingComponent
+  },
+  {
+    path : '',
+    redirectTo : 'home',
+    pathMatch : 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
